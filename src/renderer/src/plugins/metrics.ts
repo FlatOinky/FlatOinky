a7de5b391e5da72341f2d2fb5395fb7380fea335
SoggyPiggy <dev@soggypiggy.com>
2026-07-19 22:23:36 -0500

    added an icon to the metrics window button

--- src/renderer/src/plugins/metrics.ts
+++ src/renderer/src/plugins/metrics.ts
@@ -1,5 +1,8 @@
 import { Lifecycle, Plugin, PluginContext } from '../client';
 
+const windowIconSvgPath =
+	'M23 23v466h466v-18H41v-82.184l85.854-57.234 70.023 70.022 65.133-260.536L387.28 203.7l67.79-107.97 19.317 11.858 6.102-71.1-60.644 37.616 19.884 12.207-59.01 93.99-130.732-65.366-62.865 251.462-57.98-57.978L41 367.184V23H23z';
+
 type XPDrop = {
 	xp: number;
 	skill: string;
@@ -204,6 +207,10 @@ const initMetricsWindow = (
 		id: 'metrics',
 		title: 'Metrics',
 		storage: context.storages.profile,
+		icon: context.ui.createSvgIcon([windowIconSvgPath], {
+			viewBox: '0 0 512 512',
+			fill: 'currentColor',
+		}),
 		onPreMount: (window) => {
 			// window.state.minimized = true;
 			window.state.width = 192;
